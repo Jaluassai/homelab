38fd8937cb4606d01ca681ca3a03d926706b78b4
Jaluassai <guzdekpl@gmail.com>
2026-02-25 17:23:48 +0100

    added ip adresss

--- terraform/modules/lxc/outputs.tf
+++ terraform/modules/lxc/outputs.tf
@@ -5,3 +5,7 @@ output "id" {
 output "hostname" {
   value = var.hostname
 }
+
+output "ip_address" {
+  value = proxmox_virtual_environment_container.this.initialization[0].ip_config[0].ipv4[0].address
+}
