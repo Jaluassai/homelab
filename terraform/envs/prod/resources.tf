a3341832e889e0c61806ac271f96380466852a94
Jaluassai <guzdekpl@gmail.com>
2026-03-11 21:40:59 +0100

    fmt fix

--- terraform/envs/prod/resources.tf
+++ terraform/envs/prod/resources.tf
@@ -1,8 +1,8 @@
 resource "proxmox_virtual_environment_download_file" "lxc_img" {
-  content_type = "vztmpl"
-  datastore_id = "local"
-  node_name    = "Hades01"
-  url          = "http://download.proxmox.com/images/system/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+  content_type        = "vztmpl"
+  datastore_id        = "local"
+  node_name           = "Hades01"
+  url                 = "http://download.proxmox.com/images/system/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
   overwrite_unmanaged = true
   #url          = "http://download.proxmox.com/images/system/ubuntu-25.04-standard_25.04-1.1_amd64.tar.zst"
 }
