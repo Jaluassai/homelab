a4d760cfa197e7440b33173b85d16bf1adc3b03d
Jaluassai <guzdekpl@gmail.com>
2026-03-11 20:54:31 +0100

    tflint fix

--- terraform/envs/prod/provider.tf
+++ terraform/envs/prod/provider.tf
@@ -1,8 +1,17 @@
 terraform {
+  required_version = ">= 1.14.0"
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.96.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
     }
   }
 }
--- terraform/envs/prod/variables.tf
+++ terraform/envs/prod/variables.tf
@@ -3,17 +3,19 @@ variable "proxmox_api_token_id" {}
 
 variable "PROXMOX_VE_USERNAME" {
   sensitive = true
+  type        = string
 }
 
 variable "PROXMOX_VE_PASSWORD" {
   sensitive = true
+  type        = string
 }
 
-variable "proxmox_api_token_secret" {
-  description = "api token secret"
-  type        = string
-  sensitive   = true
-}
+# variable "proxmox_api_token_secret" {
+#   description = "api token secret"
+#   type        = string
+#   sensitive   = true
+# }
 
 variable "ssh_public_keys" {
   description = "Map of SSH public keys to inject into container"
