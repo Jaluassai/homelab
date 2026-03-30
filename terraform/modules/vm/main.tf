7b9d211adadd86ef98ea9f81e1bac19b919adafd
Jaluassai <guzdekpl@gmail.com>
2026-03-30 22:22:16 +0200

    removed old output

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -77,17 +77,3 @@ resource "proxmox_virtual_environment_vm" "this" {
   }
 
 }
-
-output "ubuntu_vm_password" {
-  value     = random_password.ubuntu_vm_password.result
-  sensitive = true
-}
-
-output "ubuntu_vm_private_key" {
-  value     = tls_private_key.ubuntu_vm_key.private_key_pem
-  sensitive = true
-}
-
-output "ubuntu_vm_public_key" {
-  value = tls_private_key.ubuntu_vm_key.public_key_openssh
-}
