2988d8ce4a4cc95caf8ac39d98fbfcbf658c1d3b
Jaluassai <guzdekpl@gmail.com>
2026-04-08 19:42:00 +0200

    rename of vm, removed adguard placeholder

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -1,9 +1,9 @@
-module "traefik" {
+module "Caddy" {
   source = "../../modules/lxc"
 
   node_name = "Hades01"
   vm_id     = 301
-  hostname  = "treafik"
+  hostname  = "caddy"
 
   cores  = 1
   memory = 512
@@ -25,31 +25,31 @@ module "traefik" {
 
 }
 
-module "adgurd" {
-  source = "../../modules/lxc"
+# module "adgurd" {
+#   source = "../../modules/lxc"
 
-  node_name = "Hades01"
-  vm_id     = 302
-  hostname  = "adguard"
+#   node_name = "Hades01"
+#   vm_id     = 302
+#   hostname  = "adguard"
 
-  cores  = 1
-  memory = 512
+#   cores  = 1
+#   memory = 512
 
-  ipv4_address = "192.168.100.39/24"
-  ipv4_gateway = "192.168.100.1"
+#   ipv4_address = "192.168.100.39/24"
+#   ipv4_gateway = "192.168.100.1"
 
-  datastore_id = "local-lvm"
-  disk_size    = 4
+#   datastore_id = "local-lvm"
+#   disk_size    = 4
 
-  template_file_id = proxmox_virtual_environment_download_file.lxc_img.id
-  os_type          = "ubuntu"
+#   template_file_id = proxmox_virtual_environment_download_file.lxc_img.id
+#   os_type          = "ubuntu"
 
-  password = random_password.adguard_container_password.result
-  ssh_keys = concat([tls_private_key.adguard_container_key.public_key_openssh], values(var.ssh_public_keys))
+#   password = random_password.adguard_container_password.result
+#   ssh_keys = concat([tls_private_key.adguard_container_key.public_key_openssh], values(var.ssh_public_keys))
 
-}
+# }
 
-module "docker01" {
+module "Docker01" {
   source = "../../modules/vm"
 
   node_name        = "Hades01"
