ebab6f24717b800c2906261c39d36210e031b7b1
Jaluassai <guzdekpl@gmail.com>
2026-04-08 20:25:08 +0200

    added disk interface

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -64,6 +64,7 @@ module "docker01" {
   datastore_id     = "local-lvm"
   disk_size        = 16
   disk_discard     = "on"
+  disk_interface   = "virtio"
   cloud_image_id   = proxmox_virtual_environment_download_file.vm_img.id
   password         = random_password.docker01_container_password.result
   ssh_keys         = concat([tls_private_key.docker01_container_key.public_key_openssh], values(var.ssh_public_keys))
